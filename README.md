1704268e34568f11a0e5918cce0f65057a0cd91f
Michael Vieth <mavieth@gmail.com>
2016-12-11 21:29:26 -0700

    Added text file and updated script

--- README.md
+++ README.md
@@ -13,12 +13,18 @@ git clone https://github.com/mavieth/check-domains-py.git
 cd check-domains-py
 virtualenv venv
 source venv/bin/activate
+
+pip install pythonwhois
 ```
+### Add domains to domains.txt
+Add domains (one per each line) for checker to check. It will print out availability.
 
 ### Run Script
 ```python
 python check.py
 ```
+
+
 
 
 
---
+++ check.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+import pythonwhois
+domains = []
+
+
+available = []
+unavailable = []
+
+
+def getDomains():
+    with open('domains.txt', 'r+') as f:
+        for domainName in f.read().splitlines():
+            domains.append(domainName)
+
+def run():   
+    for dom in domains:
+        if dom is not None and dom != '':
+            details = pythonwhois.get_whois(dom)
+            if details['contacts']['registrant'] is not None:
+                unavailable.append(dom)
+            else:
+                available.append(dom)
+
+def printAvailability():
+    print "-----------------------------"
+    print "Unavailable Domains: "
+    print "-----------------------------"
+    for un in unavailable:
+        print un
+    print "\n"
+    print "-----------------------------"
+    print "Available Domains: "
+    print "-----------------------------"
+    for av in available:
+        print av
+    
+
+if __name__ == "__main__":
+    getDomains()
+    run()
+    printAvailability()
+
+
